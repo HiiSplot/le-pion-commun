52166c092017f5f9f7a5e35a3cb6f23a62d59f07
Laura Bensimon <hii_splot@hotmail.fr>
2025-08-03 21:35:45 +0200

    menu page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import './App.css'
 import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import { Footer } from './Layout/footer'
 import { Header } from './Layout/header'
-import './i18n';
 import { HomePage } from './Pages/Home/HomePage';
 import { HomeHeader } from './Layout/home-header';
 import { MenuPage } from './Pages/Menu/MenuPage';
 import { ConceptPage } from './Pages/Concept/ConceptPage';
 import { ContactPage } from './Pages/Contact/ContactPage';
 import { AboutPage } from './Pages/A propos/a-propos';
+import './App.css'
+import './i18n';
 
 function Layout({ children }: { children: React.ReactNode }) {
   const location = useLocation();
--- src/Pages/Menu/MenuPage.tsx
+++ src/Pages/Menu/MenuPage.tsx
@@ -1,11 +1,1 @@
-import type React from "react";
-import './menu-page.css'
-
-export const MenuPage: React.FC = () => {
-  return (
-    <div className="menu-page">
-      <h1>Welcome to the Menu Page</h1>
-      {/* Additional content can be added here */}
-    </div>
-  );
-}
+git 
